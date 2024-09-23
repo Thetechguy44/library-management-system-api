13317362a0637bc0f19c6e84afa9f143172e8a48
Dev Gana <yahaya128173908@gmail.com>
2024-09-23 15:04:35 +0100

    created fine test feature

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/fines', [FineController::class, 'index'])->middleware('can:manage-fines');
     Route::get('/fines/{fine}', [FineController::class, 'show'])->middleware('can:manage-fines');
     Route::post('/borrow-records/{borrowRecord}/calculate-fine', [FineController::class, 'calculateFine'])->middleware('can:manage-fines');
-    Route::post('/fines/{fine}/pay', [FineController::class, 'payFine'])->middleware('can:pay-fines');
+    Route::patch('/fines/{fine}/pay', [FineController::class, 'payFine'])->middleware('can:pay-fines');
 
     //reservation
     Route::apiResource('reservations', ReservationController::class);
